703818a08120ab528093c7eb0a8c722298f16051
Anna <anasoft@yandex.ru>
2021-12-18 12:09:24 +0500

    4. Добавила кнопку в разметку и повесила на неё clickListener в Activity.

--- app/src/main/java/ru/anasoft/kotlin_1_1/MainActivity.kt
+++ app/src/main/java/ru/anasoft/kotlin_1_1/MainActivity.kt
@@ -2,10 +2,17 @@ package ru.anasoft.kotlin_1_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonOk = findViewById<Button>(R.id.button_ok)
+        buttonOk.setOnClickListener {
+            Toast.makeText(this, getString(R.string.message_text), Toast.LENGTH_LONG).show()
+        }
     }
 }
