fc821fc00aeb3a996ea694bfee1fea25297a9aa3
Anna <anasoft@yandex.ru>
2021-12-18 13:06:40 +0500

    c. Вывела значения из разных циклов в лог, используя примеры из методических материалов.

--- app/src/main/java/ru/anasoft/kotlin_1_1/MainActivity.kt
+++ app/src/main/java/ru/anasoft/kotlin_1_1/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.anasoft.kotlin_1_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -22,6 +23,24 @@ class MainActivity : AppCompatActivity() {
         val city1 = Weather("Уфа", -1)
         val city2 = city1.copy()
         textWeather.text = textWeather.text as String + "\n" + city2.toString()
+
+        // примеры циклов
+        val weatherList: List<Weather> = listOf(city1, city2)
+        for(weather in weatherList) Log.d("Anasoft", weather.toString())
+
+        for(i in 1..5) {
+            Log.d("Anasoft", "$i. $city1")
+        }
+
+        for(i in 9 downTo 1 step 2) {
+            Log.d("Anasoft", "$city1$i")
+        }
+
+        var city3 = city1.copy()
+        while (city1.equals(city3)) {
+            Log.d("Anasoft", "${city1.temperature} = ${city2.temperature}")
+            city3 = Weather("Казань", -3)
+        }
 
     }
 }
